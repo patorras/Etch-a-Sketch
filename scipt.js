dfbce03490ff277377bdd2e9b0d049faa4b745f4
patorras <triotank@gmail.com>
2021-03-12 20:07:22 +0000

    the grid is finally working

--- scipt.js
+++ scipt.js
@@ -11,34 +11,64 @@
 
 // selects the unsorted list 
 let ul = document.getElementById("mainGrid")
-let grid = 256;
+var grid = 256;
+
+
+
 
 
 
 function changeGridSize(number) {
+  
+  // funtion to delete all the div elements, this is to eleminate the grid
+  Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+  }
+  NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+  }
+
+  // receives the number of the new grid
   grid = prompt("Please enter the grid size.");
 
-  return grid = grid*grid;
-}
+  // gives the necessary style to the dom element
+  styleGrid = document.getElementById("mainGrid");
+  styleGrid.style.gridTemplateColumns = `repeat(${grid}, 1fr)`; 
+  styleGrid.style.gridTemplateRows = `repeat(${grid}, 1fr)`;
 
+  grid = grid*grid;
 
-
-// creates items
-for (i = 0; i < grid; i++) {
-  let newDiv = document.createElement("div");
-  newDiv.className = "grid";
-  newDiv.id = `coiso${i}`
-  ul.appendChild(newDiv);
-
-  // selects every new item created
-  let css = document.getElementById(`coiso${i}`);
+  // removes the grid if exists before creating a new one
+  document.getElementsByClassName("grid").remove();
   
-  // changes the background color when mouse passes by
-  css.addEventListener("mouseenter", function(e) {
-    // highlight the mouseenter target
-    e.target.style.backgroundColor = "purple";
-  });
+  // creates the grid
+  for (i = 0; i < grid; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.className = "grid";
+    newDiv.id = `coiso${i}`
+    ul.appendChild(newDiv);
+  
+
+  
+    // selects every new item created
+    let css = document.getElementById(`coiso${i}`);
+    
+    // changes the background color when mouse passes by
+    css.addEventListener("mouseenter", function(e) {
+      // highlight the mouseenter target
+      e.target.style.backgroundColor = "purple";
+    });
+  }
+
 }
+
+
+
+
 
 
 
